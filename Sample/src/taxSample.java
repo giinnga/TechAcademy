cb5fc485a0abf713bdd19cbc32cd4d455797b8ec
ginga <izuto.kudo.cop@gmail.com>
2023-09-29 02:29:17 +0900

    税込価格の計算（課題）引数taxPrice→taxRateに名称変更

--- Sample/src/taxSample.java
+++ Sample/src/taxSample.java
@@ -3,13 +3,13 @@ public class taxSample {
 
     public static void main(String[] args) {
         int price = 1500;
-        double taxprice = 1.1;
-        int result = (int) tax(price, taxprice);
-        System.out.println(price + "円の商品の税込価格は" +  result + "円（消費税は" + ((int)(price * (taxprice - 1))) + "円）です。") ;
+        double taxRate = 1.1;
+        int result = (int) tax(price, taxRate);
+        System.out.println(price + "円の商品の税込価格は" +  result + "円（消費税は" + ((int)(price * (taxRate - 1))) + "円）です。") ;
     }
 
-    public static double tax(int price, double taxprice) {
-        double result = price * taxprice;
+    public static double tax(int price, double taxRate) {
+        double result = price * taxRate;
         return result;
     }
 
