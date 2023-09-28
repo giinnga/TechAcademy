6165f9b4bdab1c2e756a53fa38f5e559795e5c6c
ginga <izuto.kudo.cop@gmail.com>
2023-09-28 21:17:14 +0900

    計算の練習

---
+++ Sample/src/Sample01.java
@@ -0,0 +1,24 @@
+
+public class Sample01 {
+
+    public static void main(String[] args) {
+        int number1;
+        String str1;
+        number1 = 1;
+        str1 = "abc";
+        System.out.println(number1);
+        System.out.println(str1);
+
+        int number2 = 100;
+        String str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+        str2 = "copen";
+        System.out.println(str2);
+
+        // 1日の秒数 = 24時間 x 60分 x 60秒
+        int ichiniti_no_byousuu = 24 * 60 * 60;    // * は 掛け算を意味する記号
+        System.out.println(ichiniti_no_byousuu);
+    }
+
+}
---
+++ Sample/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    //メインメソッドです！
+    public static void main(String[] args) {
+        /*
+        System.out.println("Kiramex");
+        System.out.println("Java");
+        System.out.println("始めましょう!");
+        */
+        System.out.println("こっちは表示される！");
+    }
+}
