08b6a668718fb6a8ee2f5232704152dab63d1ae7
ginga <izuto.kudo.cop@gmail.com>
2023-09-30 02:08:57 +0900

    不要な箇所の削除

--- Sample/src/taxSample.java
+++ Sample/src/taxSample.java
@@ -5,12 +5,11 @@ public class taxSample {
         int price = 1500;
         double taxRate = 1.1;
         int result = (int) tax(price, taxRate);
-        System.out.println(price + "円の商品の税込価格は" +  result + "円（消費税は" + ((int)(price * (taxRate - 1))) + "円）です。") ;
+        System.out.println(price + "円の商品の税込価格は" + result + "円（消費税は" + ((int)(price * (taxRate - 1))) + "円）です。");
     }
 
     public static double tax(int price, double taxRate) {
         double result = price * taxRate;
         return result;
     }
-
 }
