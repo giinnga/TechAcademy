f72432148ead25cbab9896f90780a681909866f1
ginga <izuto.kudo.cop@gmail.com>
2023-10-10 13:51:41 +0900

    @Overrideの追記

--- Sample/src/rpg/Hero.java
+++ Sample/src/rpg/Hero.java
@@ -2,15 +2,15 @@ package rpg;
 
 public class Hero extends Character {
 
-    private int wepon;
+    private int weapon;
 
 
-    public int getwepon() {
-        return wepon;
+    public int getWeapon() {
+        return weapon;
     }
 
-    public void setWeapon(int wepon) {
-        this.wepon = wepon;
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
 
     }
 
@@ -18,9 +18,26 @@ public class Hero extends Character {
 
     }
 
-    public Hero(String name, int hp, int offense, int defense, int wepon) {
+    public Hero(String name, int hp, int offense, int defense, int weapon) {
         super(name, hp, offense, defense);
-        this.wepon = wepon;
+        this.weapon = weapon;
+    }
+
+    @Override
+    public void attack(Character opponent) {
+        // (自分の攻撃力 + 自分の武器の強さ) - 相手の防御力 をダメージ量とする
+        int damage = super.getOffense() + this.getWeapon() - opponent.getDefense();
+
+        // ダメージ量の計算結果がプラスかどうか
+        if(damage > 0) {
+            // ダメージ量がプラスならダメージを与える
+            opponent.setHp(opponent.getHp() - damage);
+            System.out.println(super.getName() + "は" + opponent.getName() + "に" + damage + "のダメージを与えた！");
+        } else {
+            // ダメージ量が0以下ならミスにする
+            System.out.println("ミス！" + super.getName() + "は" + opponent.getName() + "にダメージを与えられない！");
+        }
+
     }
 
 }
