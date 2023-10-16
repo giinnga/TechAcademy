d07634c42a8dc3d21bd5b40004546391f73e0a24
ginga <izuto.kudo.cop@gmail.com>
2023-10-16 14:39:35 +0900

    エラトステネスのふるい、Scanner練習

---
+++ Sample/src/lesson6more/PrimeNumber.java
@@ -0,0 +1,24 @@
+package lesson6more;
+
+/* エラトステネスのふるい */
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        for(int i = 2; i < 100; i++) {
+            if(i != 2 && i % 2 ==0) {
+                continue;
+            }else if (i != 3 && i % 3 ==0) {
+                continue;
+            }else if (i != 5 && i % 5 ==0) {
+                continue;
+            }else if (i != 7 && i % 7 ==0) {
+                continue;
+            }else {
+                System.out.println(i);
+            }
+
+            }
+
+    }
+
+}
